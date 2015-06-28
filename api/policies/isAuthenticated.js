20304d73304c4526c23618fe10aca26eee0f5bce
amcereijo <amcereijo@gmail.com>
2015-06-28 18:18:06 +0200

    send forbiden when to logged

--- api/policies/isAuthenticated.js
+++ api/policies/isAuthenticated.js
@@ -1,8 +1,8 @@
 module.exports = function(req, res, next) {
    if (req.isAuthenticated()) {
         return next();
-    }
-    else{
-        return res.redirect('/login');
+    } else{
+    	return res.forbidden({}, 'login');
+        //return res.redirect('/login');
     }
 };
