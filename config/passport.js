337bf4574e09f418fbdb18c866f553fdafcf2c24
amcereijo <amcereijo@gmail.com>
2015-08-09 22:57:22 +0200

    using config for user and pass for admin access

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,11 @@
-var passport = require('passport'),
+var environment = process.env.ENV_VARIABLE || 'development';
+  config = require('./env/' + environment ),
+  passport = require('passport'),
 	LocalStrategy = require('passport-local').Strategy,
-	bcrypt = require('bcrypt');
+	bcrypt = require('bcrypt'),
+  USER = config.admin.user,
+  PASS = config.admin.password,
+  users = {};
 
 passport.serializeUser(function(user, done) {
     done(null, user.id);
@@ -8,19 +13,14 @@ passport.serializeUser(function(user, done) {
 
 passport.deserializeUser(function(id, done) {
 	done(null, users[id]);
-    /*User.findOne({ id: id } , function (err, user) {
-        done(err, user);
-    });*/
 });
-var USER = 'admin', PASS = 'admin';
-var users = {};
+
 
 passport.use(new LocalStrategy({
     usernameField: 'name',
     passwordField: 'password'
   },
   function(name, password, done) {
-
   	if(name === USER && password === PASS) {
   		var returnUser = {
             name: name,
@@ -35,32 +35,5 @@ passport.use(new LocalStrategy({
           message: 'Invalid login'
         });
   	}
-
-
-/*
-
-    User.findOne({ name: name }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect name.' });
-      }
-
-      bcrypt.compare(password, user.password, function (err, res) {
-          if (!res)
-            return done(null, false, {
-              message: 'Invalid Password'
-            });
-          var returnUser = {
-            name: user.name,
-            createdAt: user.createdAt,
-            id: user.id,
-            displayName: user.displayName
-          };
-          return done(null, returnUser, {
-            message: 'Logged In Successfully'
-          });
-        });
-    });
-*/
   }
 ));
